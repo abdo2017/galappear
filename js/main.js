aee08ed0a445d6d3c6e134a2f2ea3eca26b04e09
abdo2017 <abdo.esam2020@yandex.com>
2018-07-03 02:15:50 +0200

    finishing the project,Gallapear

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function () {
+    // init sideNav
+    $(".button-collapse").sideNav();
+//    init modals
+    $('.modal').modal();
+//    scrollfire
+    var options = [
+        {
+            selector: '.row-1', offset: 50, callback: function (el) {
+                Materialize.fadeInImage($(el));
+            }
+        }, {
+            selector: '.row-2', offset: 300, callback: function (el) {
+                Materialize.fadeInImage($(el));
+            }
+        }, {
+            selector: '.row-3', offset: 400, callback: function (el) {
+                Materialize.fadeInImage($(el));
+            }
+        }
+    ];
+    Materialize.scrollFire(options);
+
+
+});
